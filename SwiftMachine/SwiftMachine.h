2e32c8bd49ce5e397f8c9a732ac45fac4d1cb5a9
Akkyie <akio@prioris.org>
2015-03-07 20:44:31 +0900

    Initial Commit

---
+++ SwiftMachine/SwiftMachine.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMachine.h
+//  SwiftMachine
+//
+//  Created by Akio Yasui on 3/7/15.
+//  Copyright (c) 2015 Akio Yasui. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftMachine.
+FOUNDATION_EXPORT double SwiftMachineVersionNumber;
+
+//! Project version string for SwiftMachine.
+FOUNDATION_EXPORT const unsigned char SwiftMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMachine/PublicHeader.h>
+
+
